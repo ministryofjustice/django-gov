9b879c33bb32871f82fa5fff4181e7239597f79f
Josh Rowe <josh@s-block.com>
2016-10-31 19:44:43 +0000

    add default django settings

--- django_gov/__init__.py
+++ django_gov/__init__.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
+
+
 __version__ = (0,  0, 1)
+
+DEFAULT_SETTINGS = {
+
+}
+
+settings.configure(default_settings=DEFAULT_SETTINGS, DEBUG=True)

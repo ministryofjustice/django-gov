d7189f46d3b441668ce1a64e9bffb0aea6b1fad3
Josh Rowe <josh@s-block.com>
2016-11-01 11:46:49 +0000

    setting test

--- django_gov/__init__.py
+++ django_gov/__init__.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
-from django.conf import settings
-
-
 __version__ = (0,  0, 1)
 
-DEFAULT_SETTINGS = {
 
+DEFAULT_SETTINGS = {
+    'DEFAULT_SETTING': True,
 }
 
-settings.configure(default_settings=DEFAULT_SETTINGS, DEBUG=True)
+
+try:
+    from django.conf import settings
+    settings.configure(default_settings=DEFAULT_SETTINGS, DEBUG=True)
+except ImportError:
+    pass

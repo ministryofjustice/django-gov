54fbb17a3a0f4d216999776a302c5353bda92b7b
Josh Rowe <josh@s-block.com>
2016-11-01 16:32:53 +0000

    add default settings for included apps

--- django_gov/__init__.py
+++ django_gov/__init__.py
@@ -1,14 +1,2 @@
 # -*- coding: utf-8 -*-
 __version__ = (0,  0, 1)
-
-
-DEFAULT_SETTINGS = {
-    'DEFAULT_SETTING': True,
-}
-
-
-try:
-    from django.conf import settings
-    settings.configure(default_settings=DEFAULT_SETTINGS, DEBUG=True)
-except ImportError:
-    pass
---
+++ django_gov/settings.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
+
+API_VERSION = 'v1'
+
+SWAGGER_SETTINGS = {
+    'APIS_SORTER': 'alpha'
+}
+
+PING_JSON_KEYS = {
+    'build_date_key': 'APP_BUILD_DATE',
+    'commit_id_key': 'APP_GIT_COMMIT',
+    'version_number_key': 'APPVERSION',
+    'build_tag_key': 'APP_BUILD_TAG',
+}
+
+HEALTHCHECKS = [
+    'moj_irat.healthchecks.database_healthcheck',
+]
+
+AUTODISCOVER_HEALTHCHECKS = True
+
+CORS_ORIGIN_ALLOW_ALL = True

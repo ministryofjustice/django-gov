c9d181e21d77f8efbc1d1df7021a4787be706c35
Josh Rowe <josh@s-block.com>
2016-11-17 12:42:32 +0000

    only update if no tests specified

--- django_gov/tests/runner.py
+++ django_gov/tests/runner.py
@@ -4,7 +4,7 @@ from django.test.runner import DiscoverRunner
 
 class GovDiscoverRunner(DiscoverRunner):
     def run_tests(self, test_labels, extra_tests=None, **kwargs):
-        if 'django_gov' not in test_labels:
-            test_labels = test_labels + ('django_gov', )
+        if not test_labels and 'django_gov' not in test_labels:
+            test_labels = ('.', 'django_gov')
         return super(GovDiscoverRunner, self).run_tests(test_labels,
                                                         extra_tests, **kwargs)

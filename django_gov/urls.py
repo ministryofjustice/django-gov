a1bb1076800db2f5a1477e4886bcaee1bc7d2b20
Josh Rowe <josh@s-block.com>
2016-11-18 14:31:49 +0000

    add healthcheck urls

--- django_gov/urls.py
+++ django_gov/urls.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
+from django.conf.urls import url
+
+from moj_irat.views import PingJsonView, HealthcheckView
+
+
+urlpatterns = [
+    url(r'^ping.json$', PingJsonView.as_view(**settings.PING_JSON_KEYS),
+        name='ping_json'),
+    url(r'^healthcheck.json$', HealthcheckView.as_view(),
+        name='healthcheck_json'),
+]
